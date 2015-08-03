2e21d6c5400579672ece127bb4aaaac41f77d33a
Peter Kovacs <kiiwep@gmail.com>
2015-08-04 00:21:07 +0200

    multiple channel support added

--- persistent-amqp.js
+++ persistent-amqp.js
@@ -4,12 +4,141 @@ const EventEmitter = require('events').EventEmitter;
 const amqp = require('amqplib');
 
 const channelMethods = [
-    'close', 'assertQueue', 'checkQueue', 'deleteQueue', 'purgeQueue', 'bindQueue', 'unbindQueue', 'assertExchange',
+    'assertQueue', 'checkQueue', 'deleteQueue', 'purgeQueue', 'bindQueue', 'unbindQueue', 'assertExchange',
     'checkExchange', 'deleteExchange', 'bindExchange', 'unbindExchange', 'publish', 'sendToQueue', 'consume', 'cancel',
     'get', 'ack', 'ackAll', 'nack', 'nackAll', 'reject', 'prefetch', 'recover'
 ];
 
-class PersistentAMQP extends EventEmitter {
+let connections = {};
+
+class AMQPChannelWrapper extends EventEmitter {
+
+    constructor(connectionWrapper, isConfirmChannel, channelReopenInterval) {
+        super();
+        this._isConfirmChannel = isConfirmChannel;
+        this._openHooks = [];
+        this._closeHooks = [];
+        this._exposeChannelMethods();
+        this._connectionWrapper = connectionWrapper;
+        this._channelReopenInterval = channelReopenInterval;
+        connectionWrapper.on('connect', this._createChannel.bind(this));
+        connectionWrapper.on('terminate', this._onConnectionTerminate.bind(this));
+    }
+
+    close() {
+        const self = this;
+        if (!this._channel || this._terminating) {
+            return;
+        }
+        this._terminating = true;
+        this._executeHooks(this._closeHooks).then(() => self._channel.close()); // TODO: catch hook errors
+        this._channel.close();
+    }
+
+    get open() {
+        return this._channel !== undefined;
+    }
+
+    addOpenHook(hookFunction) {
+        this._openHooks.push(hookFunction);
+    }
+
+    addCloseHook(hookFunction) {
+        this._closeHooks.push(hookFunction);
+    }
+
+    _createChannel() {
+        const self = this;
+        if (this._channel || !this._connectionWrapper.connected) {
+            return;
+        }
+
+        let createMethodName = this._isConfirmChannel ? 'createConfirmChannel' : 'createChannel';
+        this._connectionWrapper._connection[createMethodName]().then((chan) => {
+            self._channel = chan;
+            console.log('AMQP Channel Opened');
+            chan.on('close', self._onChannelClose.bind(self));
+            chan.on('error', self._onChannelError.bind(self));
+            self._executeHooks(self._openHooks).then((res) => self.emit('open', res)); // TODO: catch hook errors
+        }).catch(err => {
+            console.error('AMQP Channel', err.toString());
+            self._scheduleNextCreateChannel();
+        });
+    }
+
+    _executeHooks(hooks) {
+        if (hooks.length === 0) {
+            return Promise.resolve();
+        }
+        let promises = [];
+        hooks.forEach(hookFn => {
+            let res = hookFn();
+            if (Array.isArray(res)) {
+                promises = promises.concat(res);
+            }
+            else {
+                promises.push(res);
+            }
+        });
+
+        return Promise.all(promises);
+    }
+
+    _onChannelClose() {
+        console.error('AMQP Channel Closed');
+        this._channel = undefined;
+        this.emit('close');
+        if (this._connectionWrapper.connected && !this._terminating) {
+            this._scheduleNextCreateChannel();
+        }
+        else if (this._connectionWrapper.connected) {
+            this._connectionWrapper._channelTerminated();
+        }
+    }
+
+    _onChannelError(err) {
+        console.error('AMQP Channel', err.toString());
+    }
+
+    _scheduleNextCreateChannel() {
+        const self = this;
+        if (this.terminating) {
+            return;
+        }
+        this._clearNextCreateChannelTimer();
+        this.nextCreateChannelTimer = setTimeout(() => {
+            self.nextCreateChannelTimer = undefined;
+            self._createChannel();
+        }, this._channelReopenInterval);
+    }
+
+    _clearNextCreateChannelTimer() {
+        if (this.nextCreateChannelTimer) {
+            clearTimeout(this.nextCreateChannelTimer);
+            this.nextCreateChannelTimer = undefined;
+        }
+    }
+
+    _onConnectionTerminate() {
+        this.close();
+    }
+
+    _exposeChannelMethods() {
+        const self = this;
+        channelMethods.forEach(methodName => {
+            self[methodName] = function () {
+                if (!self._channel) {
+                    throw new Error('Method call on a closed AMQP channel'); // TODO: queue method calls or throw error.
+                }
+                return self._channel[methodName].apply(self._channel, arguments);
+            };
+        });
+    }
+
+}
+
+class AMQPConnectionWrapper extends EventEmitter {
+
     constructor(options) {
         super();
         this._options = {
@@ -18,57 +147,57 @@ class PersistentAMQP extends EventEmitter {
             reconnectInterval: options.reconnectInterval || 1000,
             channelReopenInterval: options.channelReopenInterval || 100
         };
-        this._isConnected = false;
-        this._isOpen = false;
-        this._isTerminating = false;
+        this._channels = [];
+        this._numChannels = 0;
         this._connect();
     }
 
-    terminate() {
-        this._isTerminating = true;
-        this._clearNextConnectTimer();
-        this._clearNextCreateChannelTimer();
-        if (this._isConnected) {
-            this.connection.close();
+    createChannel(isConfirmChannel) {
+        let channel = new AMQPChannelWrapper(this, isConfirmChannel, this._options.channelReopenInterval);
+        this._numChannels++;
+        if (this._connection) {
+            channel._createChannel();
         }
+
+        return channel;
     }
 
-    isConnected() {
-        return this._isConnected;
+    createConfirmChannel() {
+        return this.createChannel(true);
     }
 
-    isOpen() {
-        return this._isOpen;
-    }
-
-    isTerminating() {
-        return this._isTerminating;
+    close() {
+        this._clearNextConnectTimer();
+        if (this._terminating && this._numChannels > 0 || !this._connection) {
+            return;
+        }
+        this._terminating = true;
+        this.emit('terminate');
+        if (this._numChannels === 0) {
+            this._connection.close();
+        }
     }
 
     _connect() {
         const self = this;
         this._clearNextConnectTimer();
-        if (this._isConnected) {
-            this._createChannel();
+        if (this._connection) {
             return;
         }
         amqp.connect(this._options.host).then(conn => {
-            self.connection = conn;
-            self._isConnected = true;
+            self._connection = conn;
             console.log('AMQP Connection Opened');
             conn.on('close', self._onConnectionClose.bind(self));
             conn.on('error', self._onConnectionError.bind(self));
-            self._createChannel();
             self.emit('connect');
         }).catch(err => {
-            console.error('AMQP Connection', err.toString()); // TODO: replace error printout
+            console.error('AMQP Connection', err.toString());
             self._scheduleNextConnect();
         });
     }
 
     _onConnectionClose() {
-        this._isConnected = false;
-        this._isOpen = false;
+        this._connection = undefined;
         console.error('AMQP Connection Closed');
         this.emit('disconnect');
         this._scheduleNextConnect();
@@ -78,47 +207,11 @@ class PersistentAMQP extends EventEmitter {
         console.error('AMQP Connection', err.toString());
     }
 
-    _createChannel() {
-        const self = this;
-
-        this._clearNextCreateChannelTimer();
-        if (this._isOpen) {
-            return;
-        }
-
-        this.connection[this._options.confirmChannel ? 'createConfirmChannel' : 'createChannel']().then(chan => {
-            self.channel = chan;
-            self._isOpen = true;
-            console.log('AMQP Channel Opened');
-            chan.on('close', self._onChannelClose.bind(self));
-            chan.on('error', self._onChannelError.bind(self));
-            self._exposeChannelMethods();
-            self.emit('open');
-        }).catch(err => {
-            console.error('AMQP Channel', err.toString()); // TODO: replace error printout
-            self._scheduleNextCreateChannel();
-        });
-    }
-
-    _onChannelClose() {
-        console.error('AMQP Channel Closed');
-        this._isOpen = false;
-        this.emit('close');
-        if (this._isConnected && !this._isTerminating) {
-            this._scheduleNextCreateChannel();
-        }
-    }
-
-    _onChannelError(err) {
-        console.error('AMQP Channel', err.toString());
-    }
-
     _scheduleNextConnect() {
         const self = this;
-        if (this._isTerminating) {
+        if (this._terminating) {
             return;
         }
-        this._clearNextCreateChannelTimer();
         this.nextConnectTimer = setTimeout(() => {
             self.nextConnectTimer = undefined;
             self._connect();
@@ -132,43 +225,32 @@ class PersistentAMQP extends EventEmitter {
         }
     }
 
-    _scheduleNextCreateChannel() {
-        const self = this;
-        if (this._isTerminating) {
-            return;
-        }
-        this.nextCreateChannelTimer = setTimeout(() => {
-            self.nextCreateChannelTimer = undefined;
-            self._createChannel();
-        }, this._options.channelReopenInterval);
-    }
-
-    _clearNextCreateChannelTimer() {
-        if (this.nextCreateChannelTimer) {
-            clearTimeout(this.nextCreateChannelTimer);
-            this.nextCreateChannelTimer = undefined;
+    _channelTerminated() {
+        this._numChannels--;
+        if (this._connection && this._numChannels === 0) {
+            console.log('AMQP Connection: all channels closed, terminating');
+            this.close();
         }
     }
 
-    _exposeChannelMethods() {
-        const self = this;
-        channelMethods.forEach(methodName => {
-            self[methodName] = function () {
-                return self.channel[methodName].apply(self.channel, arguments);
-            };
-        });
+    get connected() {
+        return this._connection !== undefined;
+    }
+
+    get terminating() {
+        return this._terminating;
     }
 }
 
-let persistentConnections = {};
+module.exports = {
+    connection(options) {
+        let opts = typeof options == 'object' ? options : {};
+        let host = opts.host || 'amqp://localhost';
+        opts.host = host;
+        if (!connections[host]) {
+            connections[host] = new AMQPConnectionWrapper(opts);
+        }
 
-module.exports = function (options) {
-    let opts = typeof options == 'object' ? opts : {};
-    let host = opts.host || 'amqp://localhost';
-    opts.host = host;
-    if (!persistentConnections[host]) {
-        persistentConnections[host] = new PersistentAMQP(opts);
+        return connections[host];
     }
-
-    return persistentConnections[host];
 };
--- test/test-client.js
+++ test/test-client.js
@@ -1,21 +1,53 @@
 'use strict';
 
-const mq = require('..')();
+const amqpConnection = require('..').connection({
+    host: 'amqp://localhost'
+});
+
+const createChannelsNum = 2;
+const simulateChannelError = true;
+
+function createChannel(idx) {
+
+    let channel = amqpConnection.createChannel();
+    let queueName;
+    let booBooTimer;
+
+    channel.addOpenHook(() => {
+        return [channel.assertQueue(null, { durable: false })]; // assert random named queue on channel create
+    });
+
+    channel.addCloseHook(() => {
+        return [channel.deleteQueue(queueName)]; // delete queue
+    });
+
+    channel.on('open', (openHookResults) => {
+        queueName = openHookResults[0].queue; // store the name of the created queue
+        console.log(`>> Channel #${idx} opened with queue named "${queueName}"`);
+
+        // create badly behaving channel user
+        if (idx === 0 && simulateChannelError) {
+            booBooTimer = setTimeout(() => {
+                channel.purgeQueue('not-existing-queue').catch((err) => {
+                    console.error('Client made some', err.toString());
+                });
+            }, 2000);
+        }
+    });
+
+    channel.on('close', () => {
+        console.log(`>> Channel #${idx} closed`);
+        if (booBooTimer) {
+            clearTimeout(booBooTimer);
+        }
+    });
+}
+
+for (let i = 0; i < createChannelsNum; i++) {
+    createChannel(i);
+}
 
 process.on('SIGINT', () => {
-    console.warn(' ~ Got SIGINT, terminating');
-    if (mq.isOpen()) {
-        mq.terminate();
-    }
-});
-
-mq.on('open', () => {
-    console.log('> got channel opened event');
-    mq.assertQueue('test1').then(queue => {
-        console.log('> queue exists/created:', queue);
-    });
-});
-
-mq.on('close', () => {
-    console.log('> got channel closed event');
+    console.warn(' ~ Terminating');
+    amqpConnection.close();
 });
